428d569e58d77b84b13500734cb55f2eb29b3228
Pedro Alves Batista <pedro.expb@gmail.com>
2018-06-07 18:11:24 -0300

    refs #36 Add a backbone for certificating account and address with a keypair

---
+++ shell-d/prototype/source/certificatecore.d
@@ -0,0 +1,37 @@
+/**
+#
+#Copyright (c) 2018 IoTone, Inc. All rights reserved.
+#
+**/
+
+module certificacore;
+
+import shelld.cryptocore;
+import shelld.addresscore;
+import shelld.accountcore;
+import shelld.persistence;
+import shelld.keypair;
+
+void captureKeypairSignature() {
+    bool keypairValidity;
+
+    if (keypairValidity) {
+
+    } else {
+        
+    }
+    
+    return null;
+}
+
+void secureCertificate() {
+    return null;
+}
+
+void generateCertificate() {
+    return null;
+}
+
+void pushCertificate() {
+    return null;
+}
