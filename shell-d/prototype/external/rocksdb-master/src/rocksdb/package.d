b99ff2205869f0ca2897da39549092dffcdda304
David J. Kordsmeier <dkords@gmail.com>
2018-06-26 08:54:03 -0700

    refs #40 Don't checkin external dependencies or tarballs that we fetch with setup-deps

--- shell-d/prototype/external/rocksdb-master/src/rocksdb/package.d
+++
@@ -1,9 +0,0 @@
-module rocksdb;
-
-public import rocksdb.batch;
-public import rocksdb.comparator;
-public import rocksdb.database;
-public import rocksdb.iterator;
-public import rocksdb.options;
-public import rocksdb.slice;
-public import rocksdb.snapshot;
