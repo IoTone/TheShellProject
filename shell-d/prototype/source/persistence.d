01f4d54e4ea0ae1c60dab712cf8625106c1cd347
David J. Kordsmeier <dkords@gmail.com>
2018-07-02 13:31:43 -0700

    refs #44 Add dddb as persistence layer module

--- shell-d/prototype/source/persistence.d
+++ shell-d/prototype/source/persistence.d
@@ -9,6 +9,7 @@ import std.stdio;
 import std.string;
 import std.file;
 import std.json;
+import dddb;
 // import rocksdb;
 
 //
@@ -97,4 +98,53 @@ class shelldb {
         // Close the database
         db.close();
     } */
+
+    unittest {
+auto db = new ddb("yes.db");
+    
+
+    // set multiple values to social_networks
+    db.set("social_networks", "facebook");
+    db.set("social_networks", "twitter");
+    db.set("social_networks", "linkedin");
+
+    writeln(db.get("social_networks"));
+    // ["facebook", "twitter", "linkedin"]
+
+    writeln(db.count("social_networks"));
+    // will return 3, because we have add ["facebook", "twitter", "linkedin"]
+
+    assert(db.count("social_networks") == 3);
+    // no error because there are 3 ["facebook", "twitter", "linkedin"]
+
+    db.update("social_networks", "facebook", "instagram");
+    // update facebook to instagram 
+    writeln(db.get("social_networks"));
+    // ["instagram", "twitter", "linkedin"]
+
+    assert(db.countkeys() == 1);
+    writeln(db.getkeys()); 
+    // db.getkeys() return a list of all keys,
+    // ["social_networks"]
+
+    assert(db.getsize() == 54);
+    writeln(db.getsize());
+    // db.getsize() return the database size in bytes
+    // 58
+    
+
+    assert(db.havevalue("social_networks", "facebook") == false);
+    writeln(db.havevalue("social_networks", "facebook")); // expected false
+    // because facebook not exists in social_networks, we update it with instagram in line 240
+
+    assert(db.havevalue("social_networks", "instagram") == true);
+    writeln(db.havevalue("social_networks", "instagram")); // expected true 
+    // true 
+
+
+    // db.drop() delete database 
+    // NOTE: all db data will be lost
+    db.drop();
+    assert(exists(db.db) == false);
+    }
 }
