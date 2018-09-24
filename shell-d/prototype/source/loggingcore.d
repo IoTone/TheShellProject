14884fa8aab38144f4474eb13ee37ab999377a52
David J. Kordsmeier <dkords@gmail.com>
2018-09-23 21:48:22 -0700

    refs #52 Fix compile issues in exceptions

--- shell-d/prototype/source/loggingcore.d
+++ shell-d/prototype/source/loggingcore.d
@@ -13,34 +13,36 @@ import core.stdc.errno;
 import shelld.persistence;
 
 class GeneralShellException : Exception {
-
-    auto exceptionHandler = new GeneralShellException;
+    this(string msg) { super(msg); }
+    // auto exceptionHandler = new GeneralShellException;
     
     string readErrorStream(string err) {
-
+        return null; // If you put in a return type it must return something
     }
 
     string printErrorStream()
-    {
-
+    { 
+        return null; // If you put in a return type it must return something
     }
 
     auto commitErrorStream() {
 
-   }
-
+    }
+    // Don't check in half baked ideas that don't compile please
+    /*
    auto retrieveErrorStream(auto errFile) {
 
    }
+   */
 
 }
 
 class ConfigurationException : GeneralShellException {
-
+    this(string msg) { super(msg); }
 }
 
 class CryptocoreException : GeneralShellException {
-    
+    this(string msg) { super(msg); }
 }
 
 unittest {}
