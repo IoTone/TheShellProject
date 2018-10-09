aba46189eb805d14f6860e5e7018ac8c246513a1
Pedro Alves Batista <pedro.expb@gmail.com>
2018-10-09 02:03:26 -0300

    refs #9 Remove yet unused code

--- shell-d/prototype/source/addresscore.d
+++ shell-d/prototype/source/addresscore.d
@@ -47,6 +47,7 @@ struct associatedBlock {
 ubyte[] generateEncoded(ubyte[] version, ubyte[] publickey) {
   ubyte[] data;
   ubyte[] step4CheckSum;
+  ubyte[] step5Concatenate;
 
   generateKey(publickey);
 
@@ -55,6 +56,8 @@ ubyte[] generateEncoded(ubyte[] version, ubyte[] publickey) {
   ubyte[] sha256PublicKey = new ubyte[crypto_hash_sha256_BYTES(pbkey)];
 
   step2Key = applyRipemd160(sha256PublicKey);
+
+  step5Concatenate 
   
   return encodedAddress;
 
@@ -76,71 +79,7 @@ int storeAddress(ubyte[] address) {
     return storeAdressStatus;
 }
 
-/*
-// Inserts the address into a pool struct for full block address stamping
-struct addressPool {
-     ubyte[] addressHeader;
-     ubyte[] addressOfNode;
-}
 
-// Some infos about the pool of networking in which the address is integrating
-struct networkPool {
-     ubyte[] address;
-
-}
-    
-// AddressCore localAddress = new AddressCore();
-
-
-ubyte[] generateAddress(ubyte[] address) {
-
-    auto hashedKey = address;
-
-        return hashedKey;
-    }
-    
-
-// Returns status about the status of the hits for the broadcast over the network
-bool BroadcastStatus() {
-        return false;
-    }
-
-auto setCheckSum() {
-        
-    }
-
-// Persist all the states to the persistence layer
-auto postAddressState() {
-
-    }
-  
-
-// Check for the local address and sync with the broadcast index of adressess
-void checkAddressIndex() {
-    
-}
-
-// Checks for the existence of the address over the network
-bool checkNetworkAddress() {
-    return false;
-}
-
-// Checks for the existence of the address over the network and fetch the genesis block to verify integrity through blockchain
-bool checkNetworkAddressBlock() {
-    return false;
-}
-
- bool verifyAddressKey() {
-        bool addressKeyIsVerified = false;
-        if (addressKeyIsVerified) {
-
-        } else {
-            writeln("Address keypair does not match");
-        }
-        
-        return false;
-    }
-*/    
 
 unittest {
         //AddressCore obj = new AddressCore();
