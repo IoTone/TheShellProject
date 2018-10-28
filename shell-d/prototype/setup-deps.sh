c5282637c309e212be6ed9dedf7d1190deb231cc
David J. Kordsmeier <dkords@gmail.com>
2018-10-27 22:00:40 -0700

    refs #64 Get rid of rockdb

--- shell-d/prototype/setup-deps.sh
+++ shell-d/prototype/setup-deps.sh
@@ -18,21 +18,21 @@ cd external
 # It does not build it.  Do that manually
 #
 # cd ..
-wget -O rocksdb-v5.12.4.tar.gz https://github.com/facebook/rocksdb/archive/v5.12.4.tar.gz
-tar -xvf rocksdb-v5.12.4.tar.gz
-cd rocksdb-5.12.4
-mkdir -p build && cd build && cmake .. && make
-cd ..
-cd ..
+# wget -O rocksdb-v5.12.4.tar.gz https://github.com/facebook/rocksdb/archive/v5.12.4.tar.gz
+# tar -xvf rocksdb-v5.12.4.tar.gz
+# cd rocksdb-5.12.4
+# mkdir -p build && cd build && cmake .. && make
+# cd ..
+# cd ..
 
 # This grabs rocksdb
 # This grabs the rocksdb d wrapper library
 # It does not build it.  It assumes the d build will do that
 #
-wget -O rocksdb.zip https://github.com/IoTone/rocksdb/archive/master.zip
-unzip rocksdb.zip
-cd rocksdb-master
-cd ..
+# wget -O rocksdb.zip https://github.com/IoTone/rocksdb/archive/master.zip
+# unzip rocksdb.zip
+# cd rocksdb-master
+# cd ..
 
 wget -O libkeccak-tiny.zip https://github.com/RomanHuryk/keccak-tiny/archive/singlefile.zip
 unzip libkeccak-tiny.zip
